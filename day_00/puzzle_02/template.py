6a93fe7b6dd4cc2fa60130f256e242f558502629
E.Motion <f.broenner@hotmail.com>
2023-12-01 14:53:06 +0100

    Updated template folder

---
+++ day_00/puzzle_02/template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+############## ## # # # #  #  #        #
+# Author: Frederik Brönner
+# Date:
+# AOC: 
+# Problem description:
+#  ---
+###### ##  ## # # # # # #     ##   #
+
+# <-- imports -->
+
+# <-- vars -->
+
+# <-- main -->
+
