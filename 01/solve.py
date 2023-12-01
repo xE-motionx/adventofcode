915c54cc5c718e134084fffab80f0814742c7082
E.Motion <f.broenner@hotmail.com>
2023-12-01 13:49:45 +0100

    perperations & day 01

---
+++ 01/solve.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+############## ## # # # #  #  #        #
+# Author: Frederik Brönner
+# Date: 2023.12.01
+# AOC:  01
+# Problem description:
+#   Rows of input. First and last diget meant to form a two-digit number
+#   Identify and combine digits to new value.
+###### ##  ## # # # # # #     ##   #
+
+# <-- imports -->
+
+# <-- vars -->
+
+# <-- main -->
+
+
