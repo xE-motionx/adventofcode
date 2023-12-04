ad2327a081ecb4ddbdbab5816e9d64dd449760bb
Frederik Brönner <f.broenner@hotmail.com>
2023-12-05 00:00:43 +0100

    Wrong day2_01

--- 2023/day_02/puzzle_01/solve.ps1
+++ 2023/day_02/puzzle_01/solve.ps1
@@ -12,5 +12,39 @@
 # <-- functions -->
 
 # <-- vars -->
+$dataSource = "./input" # "./input" # "./example"
+$maxRedCubes = 12
+$maxGreenCubes = 13
+$maxBlueCubes = 14
+$final = 0
 
 # <-- main -->
+$data = Get-Content -Path $dataSource
+foreach($game in $data){
+    $r, $b, $g = 0,0,0
+    $set = $game.split(":")
+    $draws = ($set[1].split(";"))
+    Write-Host " # --> $($set[0]) (" -NoNewLine
+    foreach($draw in $draws){
+        $splits = $draw.split(",").trim(" ")
+        foreach($part in $splits){
+            if ($part.split(" ")[1] -eq "red"){
+                $r += $part.split(" ")[0]
+            }
+            if ($part.split(" ")[1] -eq "green"){
+                $g += $part.split(" ")[0]
+            }
+            if ($part.split(" ")[1] -eq "blue"){
+                $b += $part.split(" ")[0]
+            }
+        }
+    }
+    Write-Host "Red: $($r), Green: $($g), Blue: $($b)) " -NoNewLine
+    if($r -le $maxRedCubes -and $b -le $maxBlueCubes -and $g -le $maxGreenCubes) {
+        Write-Host "Legit."
+        $final += [int] $set[0].split(" ")[1]
+    } else {
+        Write-Host "Illegal."
+    }
+}
+Write-Host "Final: $($final)"
